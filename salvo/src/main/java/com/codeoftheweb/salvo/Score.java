4b95bb145c991f8dd8031b45f33d15e236b78dff
Diego <diego_ignacio_cc@hotmail.com>
2019-09-17 20:51:18 -0300

    T5 begin

---
+++ salvo/src/main/java/com/codeoftheweb/salvo/Score.java
@@ -0,0 +1,6 @@
+package com.codeoftheweb.salvo;
+
+
+
+public class Score {
+}
--- salvo/src/main/resources/static/web/scripts/game.js
+++ salvo/src/main/resources/static/web/scripts/game.js
@@ -38,7 +38,7 @@ function getParameterByName(name) {
 
 function loadData(){
     $.get('/api/game_view/'+getParameterByName('gp'))
-        .done(function(data) {
+        .done(function(data) { console.log(data);
             let playerInfo;
             if(data.gamePlayers[0].id == getParameterByName('gp'))
                 playerInfo = [data.gamePlayers[0].player.email,data.gamePlayers[1].player.email];
@@ -57,16 +57,17 @@ function loadData(){
         });
 
 function  createGridShip(ships){
-        data.ships.forEach(function(shipPiece){
-                        shipPiece.location.forEach(function(location){
-                        $('#'+location).addClass('ship-piece');
+        ships.forEach(function(shipPiece){
+            shipPiece.location.forEach(function(location){
+                        $('#ship'+ location.toLowerCase()).addClass('ship-piece');
                         })
-                    });
-                    }
+            });
+        }
 function createGridSalvo(salvos){
              salvos.forEach(function(element){
              console.log(element);
              element.salvoLocation.forEach(function(location){
+             console.log(location);
              $('#salvo'+location.toLowerCase()).addClass('salvoPiece');
                  })
              });
